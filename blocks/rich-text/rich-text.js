7407f84dc0a098276073d2013c163d8346abf609
Dinesh <dineshks@adobe.com>
2026-05-06 23:34:30 +0530

    Add Rich Text block with full RTE and variant support

---
+++ blocks/rich-text/rich-text.js
@@ -0,0 +1,11 @@
+﻿export default function decorate(block) {
+  const row = block.querySelector(':scope > div');
+  if (!row) return;
+  const cell = row.querySelector(':scope > div');
+  if (cell) {
+    while (cell.firstChild) {
+      row.parentElement.insertBefore(cell.firstChild, row);
+    }
+    row.remove();
+  }
+}
