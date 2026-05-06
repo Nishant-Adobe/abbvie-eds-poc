ef70094492df43cea2fb62b59bd60a4020a5a3a8
Sreekanth Choudry Nalabotu <nalabotu@adobe.com>
2026-05-06 16:02:59 -0500

    Update cards-grid.js

--- blocks/cards-grid/cards-grid.js
+++ blocks/cards-grid/cards-grid.js
@@ -1,4 +1,7 @@
 const LINE1_CLASS = 'card-grid-item-line-1 abbv-icon-keyboard_arrow_right i-a';
+const LINE2_CLASS = 'card-grid-item-line-2';
+const LINE3_CLASS = 'card-grid-item-line-3';
+const LINE4_CLASS = 'card-grid-item-line-4';
 const RISA_CLASS = 'card-grid-item-risa-pri';
 
 function decorateLine1(card) {
@@ -37,6 +40,55 @@ function decorateLine1(card) {
     innerSpan.append(innerP.firstChild);
   }
   innerP.replaceWith(innerSpan);
+}
+
+/** Unwrap a single direct child `<p>` inside a line span (UE column markup). */
+function unwrapDirectParagraph(span) {
+  const lineP = span.querySelector(':scope > p');
+  if (!lineP) return;
+  while (lineP.firstChild) {
+    span.insertBefore(lineP.firstChild, lineP);
+  }
+  lineP.remove();
+}
+
+function decorateLine2(card) {
+  if (!card) return;
+  const lineDiv = [...card.children].find((el) => el.tagName === 'DIV');
+  if (!lineDiv) return;
+  const span = document.createElement('span');
+  span.className = LINE2_CLASS;
+  while (lineDiv.firstChild) {
+    span.append(lineDiv.firstChild);
+  }
+  lineDiv.replaceWith(span);
+  unwrapDirectParagraph(span);
+}
+
+function decorateLine3(card) {
+  if (!card) return;
+  const lineDiv = [...card.children].find((el) => el.tagName === 'DIV');
+  if (!lineDiv) return;
+  const span = document.createElement('span');
+  span.className = LINE3_CLASS;
+  while (lineDiv.firstChild) {
+    span.append(lineDiv.firstChild);
+  }
+  lineDiv.replaceWith(span);
+  unwrapDirectParagraph(span);
+}
+
+function decorateLine4(card) {
+  if (!card) return;
+  const lineDiv = [...card.children].find((el) => el.tagName === 'DIV');
+  if (!lineDiv) return;
+  const span = document.createElement('span');
+  span.className = LINE4_CLASS;
+  while (lineDiv.firstChild) {
+    span.append(lineDiv.firstChild);
+  }
+  lineDiv.replaceWith(span);
+  unwrapDirectParagraph(span);
 }
 
 function createWrapperATag(wrapper) {
@@ -79,8 +131,12 @@ export default function decorate(block) {
   const wrappers = [...block.querySelectorAll(':scope > div')];
   wrappers.forEach((wrapper) => {
     const gridItem = createWrapperATag(wrapper);
+    const anchor = gridItem.querySelector('a.grid-card');
 
-    decorateLine1(gridItem.querySelector('a.grid-card'));
+    decorateLine1(anchor);
+    decorateLine2(anchor);
+    decorateLine3(anchor);
+    decorateLine4(anchor);
 
     wrapper.replaceWith(gridItem);
   });
