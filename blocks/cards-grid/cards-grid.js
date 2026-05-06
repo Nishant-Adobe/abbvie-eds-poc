032cc167f41ba80bebd0b8dcd942cfe65b0d22eb
Sreekanth Choudry Nalabotu <nalabotu@adobe.com>
2026-05-06 12:30:29 -0500

    Update cards-grid.js

--- blocks/cards-grid/cards-grid.js
+++ blocks/cards-grid/cards-grid.js
@@ -1,2 +1,35 @@
-export default function decorate() {
+function createWrapperATag(wrapper) {
+  const card = document.createElement('a');
+  card.className = 'grid-card';
+  const sourceLink = wrapper.querySelector('a[href]');
+
+  if (sourceLink) {
+    card.href = sourceLink.getAttribute('href') || '#';
+    card.target = sourceLink.getAttribute('target') || '_self';
+  }
+
+  while (wrapper.firstChild) {
+    card.append(wrapper.firstChild);
+  }
+
+  const firstDiv = card.firstElementChild;
+  if (firstDiv?.tagName === 'DIV') {
+    firstDiv.remove();
+  }
+
+  const gridWrap = document.createElement('div');
+  gridWrap.className = 'card-grid-item';
+  const p = document.createElement('p');
+  p.append(card);
+  gridWrap.append(p);
+
+  return gridWrap;
+}
+
+export default function decorate(block) {
+  const wrappers = [...block.querySelectorAll(':scope > div')];
+  wrappers.forEach((wrapper) => {
+    const card = createWrapperATag(wrapper);
+    wrapper.replaceWith(card);
+  });
 }
