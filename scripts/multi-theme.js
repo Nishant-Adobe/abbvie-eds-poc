48b6fe2148f7bbed543e40814b5058f44e135bbc
Manoj Jain <manosezen@gmail.com>
2026-06-05 14:46:14 +0530

    Added sidekick config

--- scripts/multi-theme.js
+++ scripts/multi-theme.js
@@ -7,7 +7,13 @@ import {
 } from './aem.js';
 
 export function getBrandCode() {
-  return getMetadata('brand') || '';
+  return getMetadata('brand') || (document.body.classList.contains('sidekick-library') ? new URL(url).pathname.split('/')[1] : '');
+}
+
+let brandCode = getBrandCode();
+ 
+if(brandCode == '' && document.body.classList.contains('sidekick-library')) {
+  brandCode = new URL(url).pathname. Split('/')[1];
 }
 
 const brandCode = getBrandCode();
